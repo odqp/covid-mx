16a78da7f79a91869fc08c9f1b9ef5cda98fb287
Oscar David Quezada <oquezada@Itexico.onmicrosoft.com>
2020-04-02 18:40:02 -0600

    first chart

--- src/App.js
+++ src/App.js
@@ -3,20 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import ExampleBar from "./components/ExampleBar"
 
-const dataBar = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      backgroundColor: 'rgba(255,99,132,0.2)',
-      borderColor: 'rgba(255,99,132,1)',
-      borderWidth: 1,
-      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-      hoverBorderColor: 'rgba(255,99,132,1)',
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
+var dataBar;
 
 class App extends Component {
   
@@ -24,19 +11,9 @@ class App extends Component {
   {
     super()
     this.state = {
-      chartData: {}
+      isLoading: false,
+      chartData: {}      
     }
-  } 
-
-  componentWillMount(){
-    this.getChartData();
-  }
-
-  getChartData()
-  {
-    this.setState({
-      chartData: dataBar
-    });
   }
 
   render(){
@@ -49,5 +26,7 @@ class App extends Component {
       );
     }
 }
+
+
 
 export default App;
--- src/components/ExampleBar.js
+++ src/components/ExampleBar.js
@@ -1,30 +1,77 @@
 import React, { Component } from 'react';
 import {Bar} from 'react-chartjs-2';
+import axios from 'axios';
 
 class ExampleBar extends Component {
 
   constructor(props)
   {
     super(props)
+
     this.state = {
-      chartData: props.chartData
-    }
+      isLoaded: true,
+      chartData: {}
+    };
+
   }
 
-  render() {
-    return (
-      <div>
-        <h2>Bar Example (custom size)</h2>
-        <Bar
-          data={this.state.chartData}
-          width={100}
-          height={50}
-          options={{
-            maintainAspectRatio: false
-          }}
-        />
-      </div>
-    );
+  componentDidMount()
+  {
+    axios.get('https://corona.lmao.ninja/v2/historical/mx')
+    .then(response => {
+      console.log(response);
+      
+      
+
+      let recovered = response.data.timeline.cases;
+      let labels = [];
+      let values = []
+
+      for (var key in recovered) {
+        labels.push(key)
+        values.push(recovered[key])
+      }
+    
+      let finalData = {
+      labels: labels,
+      datasets: [
+          {
+            label: 'My First dataset',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            data: values
+          }
+        ]
+      };
+
+      this.setState({chartData: finalData, isLoaded: true });
+    })
+
+  }
+
+  render() {    
+    if(!this.state.isLoaded)
+    {
+
+    }
+    else{
+      return (
+        <div>
+          <h2>Bar Example (custom size)</h2>
+          <Bar
+            data={this.state.chartData}
+            width={100}
+            height={50}
+            options={{
+              maintainAspectRatio: false
+            }}
+          />
+        </div>
+      );
+    }
   }
 }
 
